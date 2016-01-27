cc160137177899b018fd9d24b4d61102efe8757f
Fayize Kaimou <fayize.kaimou@edu.univ-paris13.fr>
2016-01-27 14:54:58 +0100

    Fix the console module so it can take multiple arguments

---
+++ test/test-console-log.js
@@ -0,0 +1,9 @@
+//Test console.log with multiple arguments
+// TODO ? Compare the output of one arguments VS multiple arguements
+
+var a = "hello";
+var b = "world";
+
+console.log(a,b);
+
+slimer.exit();
